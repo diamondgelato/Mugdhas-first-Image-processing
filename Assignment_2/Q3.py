51253bb116e8b7e7ed36fed5a654c76f74a1227a
diamondgelato <mugdha.k.2001@gmail.com>
2020-06-07 15:45:46 +0530

    Update Q3.py

--- Assignment_2/Q3.py
+++ Assignment_2/Q3.py
@@ -4,25 +4,20 @@ import cv2
 import time
 
 webcam = cv2.VideoCapture (0)
-counter = 0
-#last = time.time()
+last = time.time()
 
 while True:
     x, frame = webcam.read()
     current = time.time()
 
-    if current - last > 5:
-        showFrame = cv2.flip(frame, -1)
+    if (current - last) > 2:
+        showFrame = cv2.flip (frame, -1)
         last = current
-    #else:
+    else:
         showFrame = frame
 
-    cv2.imshow ('Webcam', frame)
-    print ('{0}\n', counter)
+    cv2.imshow ('Webcam', showFrame)
+    print ((last, current))
 
-    if cv2.waitKey(1) & 0xFF == ord ('q'):
+    if cv2.waitKey (1) & 0xFF == ord ('q'):
         break
-
-    print ('Hello\n')
-
-    counter += 1
